01f3c2cbd78f5f7833ba9f4872972ca74c8e69d0
Juanlu Jimenez <juanlu.jimenez@gmail.com>
2023-12-07 20:07:19 +0100

    cambios

--- pruebaEntorno.sh
+++ pruebaEntorno.sh
@@ -4,7 +4,7 @@ TOKEN=$(curl -s \
 -u etsiiApi:laApiDeLaETSIIDaLache \
 -d "login=mariweiss@tropoli.com&password=anim" \
 -H "Content-Type: application/x-www-form-urlencoded" \
--X POST http://$SERVER:3000/api/v1/auth/login)
+-X POST http://$SERVER:4000/api/v1/auth/login)
 
 resultado=$?
 if test "$resultado" != "0"; then
@@ -13,4 +13,4 @@ fi
 
 curl \
 -H "Authorization: Bearer $TOKEN" \
-http://$SERVER:3000/api/v1/alumnos/alumno/mariweiss%40tropoli.com 
+http://$SERVER:4000/api/v1/alumnos/alumno/mariweiss%40tropoli.com 
